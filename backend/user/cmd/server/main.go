6d44ea4efbd17aa785decdd8ff047021cb94d342
hayama17 <hanetarou01@gmail.com>
2025-04-01 00:26:41 +0900

    add backend

---
+++ backend/user/cmd/server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cloudnativedaysjp/cnd-handson-app/backend/user/pkg/db"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	// データベース接続の初期化
+	if err := db.InitDB(); err != nil {
+		fmt.Println(errors.Wrap(err, "failed to initialize database"))
+		return
+	}
+	_, err := fmt.Println("hello world")
+	if err != nil {
+		fmt.Println(errors.Wrap(err, "failed to print"))
+	}
+}
