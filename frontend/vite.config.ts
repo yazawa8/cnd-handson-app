041446f41668691eceff36994ce9de30fcc4298b
Tatsuki Minami <mtatsu10@gmail.com>
2025-05-12 12:25:46 +0000

    all true

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: true,
-    port: 5173,
-    open: 'app.cicd.example.com',
-    allowedHosts: ['.app.cicd.example.com'],
+    allowedHosts: true,
   },
 })
