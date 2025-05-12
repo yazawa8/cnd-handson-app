6bac0dd2365078d3a282e4e7a6b2d02b1f986d14
Tatsuki Minami <mtatsu10@gmail.com>
2025-05-12 12:19:17 +0000

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ['app.cicd.example.com'],
+    host: true,
+    port: 5173,
+    open: 'app.cicd.example.com',
+    allowedHosts: ['.app.cicd.example.com'],
   },
 })
