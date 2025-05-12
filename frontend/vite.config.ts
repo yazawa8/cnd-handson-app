1a0bb7ebbd9bb2fee2b2fc869ea47f819f150b0f
Tatsuki Minami <mtatsu10@gmail.com>
2025-05-12 11:21:55 +0000

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],server: {
+    allowedHosts: ['app.cicd.example.com'],
+  }
 })
